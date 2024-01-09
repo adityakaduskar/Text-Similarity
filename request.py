44d7cbba194223ec81c3e992d9bd4e6efc271e2a
Aditya Kaduskar <59116309+adityakaduskar@users.noreply.github.com>
2024-01-09 18:40:39 -0500

    Added request script to work with app

---
+++ request.py
@@ -0,0 +1,11 @@
+import requests
+
+url = "http://127.0.0.1:5000/compare"
+data = {
+    "text1": "The easiest way to earn points with Fetch Rewards is to just shop for the products you already love. If you have any participating brands on your receipt, you'll get points based on the cost of the products. You don't need to clip any coupons or scan individual barcodes. Just scan each grocery receipt after you shop and we'll find the savings for you.",
+    "text2": "We are always looking for opportunities for you to earn more points, which is why we also give you a selection of Special Offers. These Special Offers are opportunities to earn bonus points on top of the regular points you earn every time you purchase a participating brand. No need to pre-select these offers, we'll give you the points whether or not you knew about the offer. We just think it is easier that way."
+}
+
+response = requests.post(url, json=data)
+
+print(response.json())
